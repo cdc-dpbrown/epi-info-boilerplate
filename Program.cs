6bf877dbdb532d9596a1bab6e063bae3ec839fd2
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-04-16 15:31:12 -0400

    Init check-in

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace epi_info_boilerplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
