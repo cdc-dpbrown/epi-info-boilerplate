dca39c12ea155152d404da3ab686079f598024ed
Knudsen <knu1@cdc.gov>
2019-04-25 18:13:45 -0400

    Menu items are now generated from an object

---
+++ Models/MenuItem.cs
@@ -0,0 +1,9 @@
+namespace Cdc.EpiInfo.Models 
+{
+    public sealed class MenuItem
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public MenuItemType Type { get; set; }
+    }
+}
---
+++ Models/MenuItemType.cs
@@ -0,0 +1,9 @@
+namespace Cdc.EpiInfo.Models 
+{
+    public enum MenuItemType
+    {
+        Header,
+        Link,
+        Divider,
+    }
+}
